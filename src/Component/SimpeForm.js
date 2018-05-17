b7d4cc859d1e83a5164130d32ff1a5723ed437fa
hwigyeompark <phg5590@gmail.com>
2018-05-17 10:31:01 +0900

    add Button, update SimpleForm

--- src/Component/SimpeForm.js
+++ src/Component/SimpeForm.js
@@ -1,15 +1,20 @@
 import React, {Component, Fragment} from 'react';
 import Input from "./Input";
+import Button from "./Button";
 
 class SimpeForm extends Component{
     state = {
         msg: ''
     };
+    
 
     render(){
         return(
             <Fragment>
-                Name <Input/>
+                Name <Input placeholder="name"/>
+                Phone <Input placeholder="phone"/>
+                Nickname <Input placeholder="nickname"/>
+                <Button title = "버튼" buttonClick={this.onClick}/>
             </Fragment>
         )
     }
