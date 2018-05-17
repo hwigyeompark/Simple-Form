1409c80b1145c14286dbeabffb45e3f6bb626f2d
hwigyeompark <phg5590@gmail.com>
2018-05-17 22:59:18 +0900

    update Input

--- src/Component/Input.js
+++ src/Component/Input.js
@@ -1,9 +1,15 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 
 class Input extends Component {
+    state = {
+        input_val : ''
+    };
     render() {
         return (
-            <div></div>
+            <Fragment>
+                <Input placeholder={this.props.placeholder} value={this.state.input_val} onChange={this.handleChange}/>
+                <div>{this.state.input_val}</div>
+            </Fragment>
         );
     }
 }
